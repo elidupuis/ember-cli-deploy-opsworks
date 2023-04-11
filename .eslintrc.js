2c703e2cef020333fca06ffc3cfe39965647305d
Eli Dupuis <eli@showbie.com>
2023-04-11 16:06:11 -0700

    Update test and linting strategy
    More inline with newer ember apps and consolidates configuration into a
    single place.
    
    This commit is based on https://github.com/elidupuis/ember-cli-deploy-archive/pull/16/commits/e941eae4fd35dbbbf27999a41a4f6f00a1997ffd.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  extends: ["eslint:recommended"],
+  env: {
+    browser: true,
+  },
+  rules: {},
+  overrides: [
+    // node files
+    {
+      files: ["./.eslintrc.js", "./index.js", "./lib/**/*.{js,ts}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ["node"],
+      extends: ["plugin:node/recommended"],
+    },
+    {
+      // Test files:
+      files: ["tests/**/*-test.{js,ts}"],
+      env: {
+        mocha: true,
+        node: true,
+      },
+    },
+  ],
+};
